23fbbb0254d7409479a56f28376dfa0d2f392da1
Husna Omer <husnaomer993@gmail.com>
2024-03-21 16:27:37 +0500

    added the icon url

--- src/weather.js
+++ src/weather.js
@@ -15,7 +15,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
       city: response.data.name,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/rain_light.png",
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
     setLoaded(true);
   }
